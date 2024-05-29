8d8ec1a1a8212421ceefba9348774da54a8f9450
singh-tanyaa <96328928+singh-tanyaa@users.noreply.github.com>
2024-05-29 17:53:56 +0530

    initial commit

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import MyProfile from "./components/Myprofile";
+import About from "./components/OrganiseForm";
+import OrganiseForm from "./components/OrganiseForm";
+
+// import { upValidation, eventValidation } from "./components/signupValidation";
+
+
+function App() {
+  return (
+    <>
+      <OrganiseForm />
+   <MyProfile/>
+      
+    </>
+  );
+}
+
+export default App;
